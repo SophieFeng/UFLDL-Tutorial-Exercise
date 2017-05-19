fd9a2bbd542acb1c8ab010a7d2902bd06c5eaea8
SophieFeng <superfengyuan@gmail.com>
2017-05-19 18:19:17 +0800

    4.SoftmaxRegression
    Softmax Regression Exercise

---
+++ 4.SoftmaxRegression/softmaxCost.m
@@ -0,0 +1,45 @@
+function [cost, grad] = softmaxCost(theta, numClasses, inputSize, lambda, data, labels)
+
+% numClasses - the number of classes 
+% inputSize - the size N of the input vector
+% lambda - weight decay parameter
+% data - the N x M input matrix, where each column data(:, i) corresponds to
+%        a single test set
+% labels - an M x 1 matrix containing the labels corresponding for the input data
+%
+
+% Unroll the parameters from theta
+theta = reshape(theta, numClasses, inputSize);
+numCases = size(data, 2);
+groundTruth = full(sparse(labels, 1:numCases, 1));
+cost = 0;
+thetagrad = zeros(numClasses, inputSize);
+
+%% ---------- YOUR CODE HERE --------------------------------------
+%  Instructions: Compute the cost and gradient for softmax regression.
+%                You need to compute thetagrad and cost.
+%                The groundTruth matrix might come in handy.
+
+thetaX = theta * data;
+thetaX = bsxfun(@minus, thetaX, max(thetaX,[],1));
+ethetaX = exp(thetaX);
+hypothesis = bsxfun(@rdivide,ethetaX,sum(ethetaX));
+sparse_hypo = log(hypothesis) .* groundTruth;
+
+decay = lambda/2 * sum(sum(theta.^2));
+cost = -1/numCases * sum(sum(sparse_hypo)) + decay;
+
+% for j = 1:numClasses
+%     vector = groundTruth(j,:) - hypothesis(j,:);
+%     weightedX = bsxfun(@times,data,vector);
+%     deltaThetaJ =  -1/numCases * sum(weightedX,2) + lambda * theta(j,:)';
+%     thetagrad(j,:) = deltaThetaJ';
+% end
+
+thetagrad = -1/numCases * (groundTruth - hypothesis) * data' + lambda * theta;
+
+% ------------------------------------------------------------------
+% Unroll the gradient matrices into a vector for minFunc
+grad = [thetagrad(:)];
+end
+
